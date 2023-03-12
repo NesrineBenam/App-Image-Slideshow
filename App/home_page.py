38497750f855f165e6cef71b996946dee4ee75e9
NesrineBenam <71776471+NesrineBenam@users.noreply.github.com>
2023-03-12 00:50:11 -0500

    Basic Layout for home-page

--- App/home_page.py
+++ App/home_page.py
@@ -6,4 +6,12 @@ from dash import Input
 from dash import Output
 import dash_bootstrap_components as dbc
 
+app = Dash(__name__, external_stylesheets= [dbc.themes.BOOTSTRAP])
+
+app.layout= dbc.Alert(
+    "Hello,", className="m-5"
+)
+
+if __name__ == '__main__' :
+    app.run_server(debug = True)
 
